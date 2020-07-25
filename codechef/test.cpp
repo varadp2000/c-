43ea82c3c5f8b45efdcae8d9fdc7f775569a540c
varadp2000 <varad@LAPTOP-JUJ8VK21.localdomain>
2020-07-25 22:17:28 +0530

    Codechef

---
+++ codechef/test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int i;
+    while(1){
+        cin  >> i ;
+        if(i ==42)
+            break;
+        if(i<100&&i>-100)
+            cout << i <<"\n";
+    }
+    return 1;
+}
