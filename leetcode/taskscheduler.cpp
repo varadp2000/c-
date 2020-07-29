98563d9411e36dd538aa5a216e7de21573667105
Varad Patil <varadpatil@gmail.com>
2020-07-29 23:23:41 +0530

    !!

---
+++ leetcode/taskscheduler.cpp
@@ -0,0 +1,20 @@
+//Solution for Leetcode weakly coding challange problem Task Scheduler
+//  28/07/2020
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<char> tasks;
+    for (int i = 0; i < n;i++){
+        int i = 0;
+        cin << i;
+        tasks.push_back(i);
+    }
+    int time = 0;
+    char currTask;
+    for (int i = 0; i < n;i++){
+    }
+}
